9084733feb4a0240b323c4ddaf545cce9e4a88b6
Abdulrahmanhaney <you@example.com>
2023-06-26 02:30:02 +0300

    adding splashview animation

--- lib/Features/Splash/persentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/persentation/views/widgets/splash_view_body.dart
@@ -2,8 +2,42 @@ import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/generated/l10n.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+  late Animation<double> fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+    fadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +45,27 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
-        Text(
-          S.of(context).SplashText,
-          textAlign: TextAlign.center,
+        AnimatedBuilder(
+          animation: fadeAnimation,
+          builder: (context, _) {
+            return FadeTransition(
+              opacity: fadeAnimation,
+              child: Image.asset(AssetsData.logo),
+            );
+          },
+        ),
+        AnimatedBuilder(
+          animation: fadeAnimation,
+          builder: (context, child) => FadeTransition(
+            opacity: fadeAnimation,
+            child: SlideTransition(
+              position: slideAnimation,
+              child: Text(
+                S.of(context).SplashText,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
         ),
       ],
     );
