b94083cd3be9f4543ec67b2d7ed410e9bdb30f04
Abdulrahmanhaney <you@example.com>
2023-06-26 15:56:52 +0300

    add font family

--- lib/core/utils/themes.dart
+++ lib/core/utils/themes.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Themes {
   static ThemeData mainTheme() => ThemeData(
@@ -7,6 +8,6 @@ class Themes {
         scaffoldBackgroundColor: kPrimaryColor,
         primaryColor: kPrimaryColor,
         brightness: Brightness.dark,
-        textTheme: const TextTheme(),
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       );
 }
