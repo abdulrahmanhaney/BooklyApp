e0f01b7b82664784c57bcdc93f344dcbc84677f3
Abdulrahmanhaney <you@example.com>
2023-06-28 16:31:24 +0300

    create api service

---
+++ lib/Features/Home/data/repos/home_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -2,5 +2,5 @@ class AssetsData {
   static const logo = 'assets/images/Logo.png';
   static const searchIcon = 'assets/images/ic_search.png';
   static const testImage =
-      'https://i.pinimg.com/564x/a2/0f/ed/a20fedf3c81c1aff08b9eaf779442bd0.jpg';
+      'http://books.google.com/books/content?id=7g5WU-D8RkIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api';
 }
