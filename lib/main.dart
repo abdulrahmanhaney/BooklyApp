7b43a74c64d8abac566ec16e89b7faafa8cbc5d7
Abdulrahmanhaney <you@example.com>
2023-06-29 12:43:46 +0300

    add setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:bookly_app/Features/Home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/Home/presentation/manage/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/Home/presentation/manage/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/Features/Search/presentation/views/search_view.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/core/utils/styles/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'Features/Home/presentation/views/book_detail_view.dart';
 import 'Features/Home/presentation/views/home_view.dart';
@@ -8,6 +13,7 @@ import 'Features/Splash/presentation/view/splash_view.dart';
 import 'generated/l10n.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -16,25 +22,35 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        SplashView.id: (context) => const SplashView(),
-        HomeView.id: (context) => const HomeView(),
-        BookDetailsView.id: (context) => const BookDetailsView(),
-        SearchView.id: (context) => const SearchView(),
-      },
-      initialRoute: SplashView.id,
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
       ],
-      supportedLocales: S.delegate.supportedLocales,
-      locale: const Locale('en'),
-      theme: Themes.mainTheme(),
-      home: const SplashView(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          SplashView.id: (context) => const SplashView(),
+          HomeView.id: (context) => const HomeView(),
+          BookDetailsView.id: (context) => const BookDetailsView(),
+          SearchView.id: (context) => const SearchView(),
+        },
+        initialRoute: SplashView.id,
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        locale: const Locale('en'),
+        theme: Themes.mainTheme(),
+        home: const SplashView(),
+      ),
     );
   }
 }
